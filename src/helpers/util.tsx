1f797570e2e3dd8c76dc8d73961ebe2307109579
benfogiel <benjaminwfogiel@gmail.com>
2024-06-15 19:01:22 -0700

    satRecToGeodetic util func

--- src/helpers/util.tsx
+++ src/helpers/util.tsx
@@ -1,18 +1,34 @@
-import {twoline2satrec, propagate, SatRec} from "satellite.js";
-
-/* Given a SatRec and a time, output the ECI X,Y,Z coordinates. */
-export const satRecToEci = (satRec: SatRec, time: Date)
-    : { x: number, y: number, z: number } | null => {
-    const posEci = propagate(satRec, time).position;
-    if (!posEci) {
-    // throw new Error('Error propagating satellite position');
-        return null;
-    }
-    return { x: posEci.x, y: posEci.y, z: posEci.z };
-};
+import {
+    twoline2satrec,
+    propagate,
+    gstime,
+    eciToGeodetic,
+    SatRec,
+    GeodeticLocation,
+    EciVec3,
+} from "satellite.js";
 
 /* Given a TLE, output the SatRec object. */
 export const tleToSatRec = (line1: string, line2: string) : SatRec => {
     return twoline2satrec(line1, line2);
+};
+
+/* Given a SatRec and a time, output the ECI X,Y,Z coordinates. */
+export const satRecToEci = (satRec: SatRec, time: Date): EciVec3<number> | null => {
+    const posEci = propagate(satRec, time).position;
+    if (!posEci) {
+        return null;
+    }
+    return posEci;
+};
+
+/* Given a SatRec and a time, output the Geodetic coordinates. */
+export const satRecToGeodetic = (satRec: SatRec, time: Date): GeodeticLocation | null => {
+    const gmst = gstime(time);
+    const posEci = propagate(satRec, time).position;
+    if (!posEci) {
+        return null;
+    }
+    return eciToGeodetic(posEci, gmst);
 };
 
