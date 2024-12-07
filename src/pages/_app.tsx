5a3dce3f46c524fa66c4f2ce10e6811944925779
benfogiel <benjaminwfogiel@gmail.com>
2024-12-07 15:52:28 -0800

    fix deprecated meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         href="/logo512.png"
       />
       <link rel="manifest" href="/manifest.json" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes"/>
       <meta
         name="apple-mobile-web-app-status-bar-style"
         content="black"
