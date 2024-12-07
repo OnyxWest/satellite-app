b37ca0acf1d165e29524b5eb69c8f6b2556e1119
benfogiel <benjaminwfogiel@gmail.com>
2024-12-07 15:04:15 -0800

    move leo_sat data

--- src/helpers/services/tle.tsx
+++ src/helpers/services/tle.tsx
@@ -2,10 +2,9 @@
 import {SatRec} from "satellite.js"
 
 import {tleToSatRec} from "../util";
+import leo_sats from "../../tmp_data/leo_sats.json";
 
-export const getTles = async (): Promise<SatRec[]> => {
-    const response = await fetch("/leo_sats.json");
-    const result = await response.json();
-    const satRecs: SatRec[] = result.map((s: {line1: string; line2: string}) => tleToSatRec(s.line1, s.line2));
+export const getTles = (): SatRec[] => {
+    const satRecs: SatRec[] = leo_sats.map((s: {line1: string; line2: string}) => tleToSatRec(s.line1, s.line2));
     return satRecs;
 };
