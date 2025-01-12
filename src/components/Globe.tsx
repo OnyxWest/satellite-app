f3181566693ead748ea84eee1eb2add08e51172c
benfogiel <benjaminwfogiel@gmail.com>
2025-01-12 12:23:39 -0800

    Add cesium access token to Globe/Cesium component

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,10 +1,17 @@
 "use client";
 import React from "react";
 import { Viewer } from "resium";
+import { Ion } from "cesium";
 import LaunchSites from "./LaunchSites";
 import SatelliteCollection from "./SatelliteCollection";
 
-export default function Cesium() {
+export default function Globe() {
+  if (process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN) {
+    Ion.defaultAccessToken = process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN;
+  } else {
+    console.error("NEXT_PUBLIC_CESIUM_ACCESS_TOKEN is not set");
+  }
+
   return (
     <Viewer
       full
