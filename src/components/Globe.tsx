d54f2872e7cd393358a1867515d3cefe14f51eb6
OnyxWest <cyruszegreat@gmail.com>
2025-02-25 17:31:36 -0800

    Use custom skybox

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,23 +1,39 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Viewer } from "resium";
+import * as Cesium from "cesium";
 import { Ion } from "cesium";
 import LaunchSites from "./LaunchSites";
 import SatelliteCollection from "./SatelliteCollection";
 
 export default function Globe() {
-  if (process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN) {
-    Ion.defaultAccessToken = process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN;
-  } else {
-    console.error("NEXT_PUBLIC_CESIUM_ACCESS_TOKEN is not set");
-  }
+	const [skyBox, setSkyBox] = useState(null);
+	if (process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN) {
+		Ion.defaultAccessToken = process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN;
+	} else {
+		console.error("NEXT_PUBLIC_CESIUM_ACCESS_TOKEN is not set");
+	}
+	useEffect(() => {
+		setSkyBox(
+			new Cesium.SkyBox({
+				sources: {
+					positiveX: "right.png",
+					negativeX: "left.png",
+					positiveY: "bottom.png",
+					negativeY: "top.png",
+					positiveZ: "front.png",
+					negativeZ: "back.png",
+				},
+			})
+		);
+	}, []);
 
-  return (
-    <Viewer
-      full
-    >
-      <LaunchSites />
-      <SatelliteCollection batch_size={1000} update_rate={75}/>
-    </Viewer>
-  );
+	return (
+		<>
+			<Viewer full skyAtmosphere={false} skyBox={skyBox}>
+				<LaunchSites />
+				<SatelliteCollection batch_size={1000} update_rate={75} />
+			</Viewer>
+		</>
+	);
 }
