2b44585b927cece87ef4e9efc55d6cbd7f20da24
OnyxWest <cyruszegreat@gmail.com>
2025-02-27 17:27:16 -0800

    min,max camera zoom

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,39 +1,37 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { Viewer } from "resium";
+import React from "react";
+import { Scene, ScreenSpaceCameraController, SkyBox, Viewer } from "resium";
 import * as Cesium from "cesium";
-import { Ion } from "cesium";
 import LaunchSites from "./LaunchSites";
 import SatelliteCollection from "./SatelliteCollection";
 
 export default function Globe() {
-	const [skyBox, setSkyBox] = useState(null);
 	if (process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN) {
-		Ion.defaultAccessToken = process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN;
+		Cesium.Ion.defaultAccessToken =
+			process.env.NEXT_PUBLIC_CESIUM_ACCESS_TOKEN;
 	} else {
 		console.error("NEXT_PUBLIC_CESIUM_ACCESS_TOKEN is not set");
 	}
-	useEffect(() => {
-		setSkyBox(
-			new Cesium.SkyBox({
-				sources: {
+
+	return (
+		<Viewer full skyAtmosphere={false}>
+			<ScreenSpaceCameraController
+				maximumZoomDistance={300000000}
+				minimumZoomDistance={500000}
+			/>
+			<SkyBox
+				show={true}
+				sources={{
 					positiveX: "right.png",
 					negativeX: "left.png",
 					positiveY: "bottom.png",
 					negativeY: "top.png",
 					positiveZ: "front.png",
 					negativeZ: "back.png",
-				},
-			})
-		);
-	}, []);
-
-	return (
-		<>
-			<Viewer full skyAtmosphere={false} skyBox={skyBox}>
-				<LaunchSites />
-				<SatelliteCollection batch_size={1000} update_rate={75} />
-			</Viewer>
-		</>
+				}}
+			/>
+			<LaunchSites />
+			<SatelliteCollection batch_size={1000} update_rate={75} />
+		</Viewer>
 	);
 }
--- src/components/SatelliteCollection.tsx
+++ src/components/SatelliteCollection.tsx
@@ -1,109 +1,140 @@
 "use client";
-import React, {useEffect, useRef, useState, FC, MutableRefObject} from "react";
+import React, {
+	useEffect,
+	useRef,
+	useState,
+	FC,
+	MutableRefObject,
+} from "react";
 import { Cartesian3, Color, Transforms } from "cesium";
 import { PointPrimitiveCollection, CesiumComponentRef } from "resium";
 import { SatRec } from "satellite.js";
 import { PointPrimitive } from "cesium";
 
-import {satRecToEci} from "../helpers/util";
-import {getTles} from "../helpers/services/tle"
+import { satRecToEci } from "../helpers/util";
+import { getTles } from "../helpers/services/tle";
 
 export interface SatelliteCollectionProps {
-  batch_size: number;
-  update_rate: number;
+	batch_size: number;
+	update_rate: number;
 }
 
 const SatelliteCollection: FC<SatelliteCollectionProps> = (props) => {
-  const satRecsRef = useRef<SatRec[]>([]);
-  const positionsRef = useRef<(Cartesian3 | null)[]>([]);
-  const batchIndexRef = useRef(0);
-  const pointsRef = useRef<(PointPrimitive | null)[]>([]);
-  const collectionRef: MutableRefObject<CesiumComponentRef<typeof PointPrimitiveCollection> | null> = useRef(null);
+	const satRecsRef = useRef<SatRec[]>([]);
+	const positionsRef = useRef<(Cartesian3 | null)[]>([]);
+	const batchIndexRef = useRef(0);
+	const pointsRef = useRef<(PointPrimitive | null)[]>([]);
+	const collectionRef: MutableRefObject<CesiumComponentRef<
+		typeof PointPrimitiveCollection
+	> | null> = useRef(null);
 
-  const [collectionsMounted, setCollectionsMounted] = useState(false)
+	const [collectionsMounted, setCollectionsMounted] = useState(false);
 
-  const addPoints = () => {
-    const fetchData = async () => {
-      const satRecs = await getTles();
-      satRecsRef.current = satRecs;
+	const addPoints = () => {
+		const fetchData = async () => {
+			const satRecs = await getTles();
+			satRecsRef.current = satRecs;
 
-      // initial positions
-      const initialPositions = satRecs.map((satRec: SatRec) => {
-        const position = satRecToEci(satRec, new Date());
-        return position ? new Cartesian3(position.x * 1000, position.y * 1000, position.z * 1000) : null;
-      });
-      positionsRef.current = initialPositions;
+			// initial positions
+			const initialPositions = satRecs.map((satRec: SatRec) => {
+				const position = satRecToEci(satRec, new Date());
+				return position
+					? new Cartesian3(
+							position.x * 1000,
+							position.y * 1000,
+							position.z * 1000
+						)
+					: null;
+			});
+			positionsRef.current = initialPositions;
 
-      const pointPrimitives = satRecs.map((_satRec, i) => {
-        if (initialPositions[i]) {
-          // @ts-expect-error - cesiumElement props does not include add method
-          return collectionRef.current?.cesiumElement?.add({
-            position: initialPositions[i],
-            pixelSize: 2.5,
-            color: Color.WHITE,
-          });
-        }
-        return null;
-      });
-      pointsRef.current = pointPrimitives;
-    };
+			const pointPrimitives = satRecs.map((_satRec, i) => {
+				if (initialPositions[i]) {
+					// @ts-expect-error - cesiumElement props does not include add method
+					return collectionRef.current?.cesiumElement?.add({
+						position: initialPositions[i],
+						pixelSize: 2.5,
+						color: Color.WHITE,
+					});
+				}
+				return null;
+			});
+			pointsRef.current = pointPrimitives;
+		};
 
-    fetchData();
-  };
+		fetchData();
+	};
 
-  const updateSatellitePositions = (satRecs: SatRec[], time: Date, start: number, end: number) => {
-    for (let i = start; i < end; i++) {
-      const satRec = satRecs[i];
-      const position = satRecToEci(satRec, time);
-      if (position && pointsRef && pointsRef.current?.[i]) {
-        // @ts-expect-error - ts being stupid
-        pointsRef.current[i].position = new Cartesian3(position.x * 1000, position.y * 1000, position.z * 1000);
-      }
-    }
-  };
+	const updateSatellitePositions = (
+		satRecs: SatRec[],
+		time: Date,
+		start: number,
+		end: number
+	) => {
+		for (let i = start; i < end; i++) {
+			const satRec = satRecs[i];
+			const position = satRecToEci(satRec, time);
+			if (position && pointsRef && pointsRef.current?.[i]) {
+				// @ts-expect-error - ts being stupid
+				pointsRef.current[i].position = new Cartesian3(
+					position.x * 1000,
+					position.y * 1000,
+					position.z * 1000
+				);
+			}
+		}
+	};
 
-  useEffect(() => {
-    if (
-      collectionsMounted &&
-        collectionRef.current?.cesiumElement &&
-        // @ts-expect-error - cesiumElement props does not include add isDestroyed method
-        !collectionRef.current.cesiumElement.isDestroyed()
-    ) {
-      addPoints();
-    }
-  }, [collectionsMounted, collectionRef.current]);
+	useEffect(() => {
+		if (
+			collectionsMounted &&
+			collectionRef.current?.cesiumElement &&
+			// @ts-expect-error - cesiumElement props does not include add isDestroyed method
+			!collectionRef.current.cesiumElement.isDestroyed()
+		) {
+			addPoints();
+		}
+	}, [collectionsMounted, collectionRef.current]);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (
-        !collectionsMounted &&
-                // @ts-expect-error - cesiumElement is not defined in type
-                (satRecsRef.current.length === 0 || !collectionRef.current.cesiumElement.length)
-      ) {
-        setCollectionsMounted(true);
-        return;
-      }
-      const time = new Date();
-      const start = batchIndexRef.current * props.batch_size;
-      const end = Math.min(start + props.batch_size, satRecsRef.current.length);
-      updateSatellitePositions(satRecsRef.current, time, start, end);
+	useEffect(() => {
+		const timer = setInterval(() => {
+			if (
+				!collectionsMounted &&
+				// @ts-expect-error - cesiumElement is not defined in type
+				(satRecsRef.current.length === 0 ||
+					!collectionRef.current.cesiumElement.length)
+			) {
+				setCollectionsMounted(true);
+				return;
+			}
+			const time = new Date();
+			const start = batchIndexRef.current * props.batch_size;
+			const end = Math.min(
+				start + props.batch_size,
+				satRecsRef.current.length
+			);
+			updateSatellitePositions(satRecsRef.current, time, start, end);
 
-      // Update batch index cyclically
-      batchIndexRef.current = (batchIndexRef.current + 1) % Math.ceil(satRecsRef.current.length / props.batch_size);
-    }, props.update_rate);
+			// Update batch index cyclically
+			batchIndexRef.current =
+				(batchIndexRef.current + 1) %
+				Math.ceil(satRecsRef.current.length / props.batch_size);
+		}, props.update_rate);
 
-    return () => {
-      clearInterval(timer);
-    }
-  }, [updateSatellitePositions]);
+		return () => {
+			clearInterval(timer);
+		};
+	}, [updateSatellitePositions]);
 
-  return (
-    <PointPrimitiveCollection
-      // @ts-expect-error - ts being stupid
-      ref={collectionRef}
-      modelMatrix={Transforms.eastNorthUpToFixedFrame(new Cartesian3(0, 0, 0))}
-    />
-  );
-}
+	return (
+		<PointPrimitiveCollection
+			// @ts-expect-error - ts being stupid
+			ref={collectionRef}
+			modelMatrix={Transforms.eastNorthUpToFixedFrame(
+				new Cartesian3(0, 0, 0)
+			)}
+		/>
+	);
+};
 
 export default SatelliteCollection;
